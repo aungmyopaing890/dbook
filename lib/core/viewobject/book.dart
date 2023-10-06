38ade88392049210e3c34a076e8fe2878ee624d8
aungmyopaing <aungmyopaing890@gmail.com>
2023-10-06 09:16:11 +0630

    book view model created

---
+++ lib/core/viewobject/book.dart
@@ -0,0 +1,67 @@
+class BookData {
+  BookData({
+    this.success,
+    this.message,
+    this.data,
+  });
+  bool? success;
+  String? message;
+  List<Book>? data;
+
+  BookData fromMap(Map<String, dynamic> dynamicData) {
+    return BookData(
+      success: dynamicData['status'],
+      message: 'Success',
+      data: Book().fromMapList(dynamicData['data']),
+    );
+  }
+}
+
+class Book {
+  Book({this.title, this.authors, this.subtitle, this.image, this.url});
+
+  String? title;
+  String? authors;
+  String? subtitle;
+  String? image;
+  String? url;
+  Book fromMap(dynamic dynamicData) {
+    return Book(
+      title: dynamicData["title"],
+      authors: dynamicData["authors"],
+      subtitle: dynamicData["subtitle"],
+      image: dynamicData["image"],
+      url: dynamicData["url"],
+    );
+  }
+
+  Map<String, dynamic>? toMap(Book object) {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = object.title;
+    data['authors'] = object.authors;
+    data['subtitle'] = object.subtitle;
+    data['image'] = object.image;
+    data['url'] = object.url;
+    return data;
+  }
+
+  List<Book> fromMapList(List<dynamic> dynamicDataList) {
+    final List<Book> subUserList = <Book>[];
+    for (dynamic dynamicData in dynamicDataList) {
+      if (dynamicData != null) {
+        subUserList.add(fromMap(dynamicData));
+      }
+    }
+    return subUserList;
+  }
+
+  List<Map<String, dynamic>?> toMapList(List<Book> objectList) {
+    final List<Map<String, dynamic>?> mapList = <Map<String, dynamic>?>[];
+    for (Book? data in objectList) {
+      if (data != null) {
+        mapList.add(toMap(data));
+      }
+    }
+    return mapList;
+  }
+}
